94c79755a369429cfb03bc0987fd2a3b66b607d5
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-25 13:29:49 +0300

    remove duplicated order attribute (total:string)

--- db/migrate/20240125102042_create_admin_orders.rb
+++ db/migrate/20240125102042_create_admin_orders.rb
@@ -4,7 +4,6 @@ class CreateAdminOrders < ActiveRecord::Migration[7.0]
       t.string :customer_email
       t.boolean :fulfilled
       t.integer :total
-      t.integer :total
       t.string :adress
 
       t.timestamps
