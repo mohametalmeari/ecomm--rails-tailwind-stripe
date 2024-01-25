c2702e035ee55df7d4ef9d8f59acb9d5cb0b63d1
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-25 23:18:37 +0300

    Fix chart.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "chart.js" # @4.4.1
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.1/dist/chart.js" # @4.4.1
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
