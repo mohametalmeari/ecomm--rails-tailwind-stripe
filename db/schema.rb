d4c8bf9aad59f3c25e26d13da4e12e26c64297dd
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-25 13:30:20 +0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_081404) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_102042) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_081404) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_email"
+    t.boolean "fulfilled"
+    t.integer "total"
+    t.string "adress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
