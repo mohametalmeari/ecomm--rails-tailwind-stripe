efec3e53c66eff073bc8fd57b4b3d76e01570cb8
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-27 02:21:08 +0300

    Test stimulus products

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="products"
 export default class extends Controller {
   connect() {
+    console.log("Hello, Stimulus!", this.element)
   }
 }
