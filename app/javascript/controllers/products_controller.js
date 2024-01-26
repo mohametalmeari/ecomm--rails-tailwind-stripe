42addce50a14fc1fef6c742ab4c3b49bcf16ec86
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-27 02:46:33 +0300

    Add functionality to add products to cart and display selected size

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,7 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="products"
 export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!", this.element)
+  static values = { size: String, product: Object }
+
+  addToCart() {
+    const cart = localStorage.getItem("cart")
+    if (cart) {
+      const cartArray = JSON.parse(cart)
+      const foundIndex = cartArray.findIndex(item => item.id === this.productValue.id && item.size === this.sizeValue)
+      if (foundIndex >= 0) {
+        cartArray[foundIndex].quantity = parseInt(cartArray[foundIndex].quantity) + 1
+      } else {
+        cartArray.push({
+          id: this.productValue.id,
+          name: this.productValue.name,
+          price: this.productValue.price,
+          size: this.sizeValue,
+          quantity: 1
+        })
+      }
+      localStorage.setItem("cart", JSON.stringify(cartArray))
+    } else {
+      const cartArray = []
+      cartArray.push({
+        id: this.productValue.id,
+        name: this.productValue.name,
+        price: this.productValue.price,
+        size: this.sizeValue,
+        quantity: 1
+      })
+      localStorage.setItem("cart", JSON.stringify(cartArray))
+    }
+  }
+
+  selectSize(e) {
+    this.sizeValue = e.target.value
+    const selectedSizeEl = document.getElementById("selected-size")
+    selectedSizeEl.innerHTML = `Selected size: ${this.sizeValue}`
   }
 }
